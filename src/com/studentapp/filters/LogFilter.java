562739b7d535a30f025bc3353a3b2c7ea33ce7d8
db90 <daniil.bigma@gmail.com>
2020-01-03 01:16:19 +0100

    small change

--- src/com/studentapp/filters/LogFilter.java
+++ src/com/studentapp/filters/LogFilter.java
@@ -1,14 +1,19 @@
 package com.studentapp.filters;
 
-//Import required java libraries
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.util.*;
+import java.util.Date;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 //Implements Filter class
 public class LogFilter implements Filter {
 	public void init(FilterConfig config) throws ServletException {
+		System.out.println("LogFilter init!");
 		// Get init parameter
 		String testParam = config.getInitParameter("test-param");
 
@@ -18,15 +23,27 @@ public class LogFilter implements Filter {
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws java.io.IOException, ServletException {
+		
+        HttpServletRequest req = (HttpServletRequest) request;
+        
+        String servletPath = req.getServletPath();
+ 
+        System.out.println("#INFO " + new Date());
+        System.out.println("ServletPath: " + servletPath);  
+        System.out.println("ContextPath " + req.getContextPath());
+        System.out.println("URL => " + req.getRequestURL());
+        System.out.println("QueryString => " + req.getQueryString());
+        System.out.println("---------------------------------------------------");
 
 		// Get the IP address of client machine.
-		String ipAddress = request.getRemoteAddr();
-
-		// Log the IP address and current timestamp.
-		System.out.println("IP " + ipAddress + ", Time " + new Date().toString());
-		System.out.println("---------------------------------------------------");
+//		String ipAddress = request.getRemoteAddr();
+//
+//		// Log the IP address and current timestamp.
+//		System.out.println("IP " + ipAddress + ", Time " + new Date().toString());
+//		System.out.println("---------------------------------------------------");
 
 		// Pass request back down the filter chain
+		// Разрешить request продвигаться дальше. (Перейти данный Filter).
 		chain.doFilter(request, response);
 	}
 
@@ -35,5 +52,6 @@ public class LogFilter implements Filter {
 		 * Called before the Filter instance is removed from service by the web
 		 * container
 		 */
+		System.out.println("LogFilter destroy!");
 	}
 }
