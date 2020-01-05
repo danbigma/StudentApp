e2f33032a35b7c4d0517e5da9cbfea6562f9f282
db90 <daniil.bigma@gmail.com>
2020-01-05 15:26:53 +0100

    login filter

--- src/com/studentapp/controller/Login.java
+++ src/com/studentapp/controller/Login.java
@@ -1,24 +1,51 @@
 package com.studentapp.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/login")
 public class Login extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
+	
+	private final String username = "admin";
+    private final String password = "admin";
+    
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
 
+        // get request parameters for username and password
+        String username = request.getParameter("login");
+        String password = request.getParameter("password");
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		
-		response.sendRedirect("studentController");
-        
-	}
+        if (this.username.equals(username) && this.password.equals(password)) {
+            //get the old session and invalidate
+            HttpSession oldSession = request.getSession(false);
+            if (oldSession != null) {
+                oldSession.invalidate();
+            }
+            //generate a new session
+            HttpSession newSession = request.getSession(true);
+
+            //setting session to expiry in 5 mins
+            newSession.setMaxInactiveInterval(5*60);
+
+            Cookie message = new Cookie("message", "Welcome");
+            response.addCookie(message);
+    		response.sendRedirect("studentController");
+        } else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.jsp");
+            rd.include(request, response);
+        }
+    }
 
 }
--- src/com/studentapp/filters/LoginFilter.java
+++ src/com/studentapp/filters/LoginFilter.java
@@ -5,14 +5,18 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
-import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebFilter(filterName = "/loginfilter", urlPatterns = { "/loginfilter" })
 public class LoginFilter implements Filter {
+	private ServletContext context;
 
 	public LoginFilter() {
 	}
@@ -21,13 +25,27 @@ public class LoginFilter implements Filter {
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {		
-
-		chain.doFilter(request, response);
+			throws IOException, ServletException {
 		
+		HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        HttpSession session = req.getSession(false);
+
+        if (session == null) {   //checking whether the session exists
+            this.context.log("Unauthorized access request");
+            res.sendRedirect(req.getContextPath() + "/login.jsp");
+        } else {
+            // pass the request along the filter chain
+            chain.doFilter(request, response);
+        }
+
 	}
 
 	public void init(FilterConfig fConfig) throws ServletException {
+        this.context = fConfig.getServletContext();
+        this.context.log("AuthenticationFilter initialized");
+		System.out.println("Login filter init!");
 	}
 
 }
